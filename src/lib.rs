bce90c8e171572f96a0c7d3f93d7dff66c4802c5
DigitalCyan <marinoljubetic8@gmail.com>
2023-01-04 19:28:06 +0100

    Added new functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
 mod tests;
 
 use std::any::TypeId;
+use std::fmt::Display;
 use std::io::stdin;
+use std::io::stdout;
 use std::io::BufRead;
+use std::io::Write;
 use std::str::FromStr;
 
 /// Read user input from `stdin` and try to parse it into a generic type.
-/// 
+///
 /// # Example
 /// ```rust
-/// use read_stdin::read_stdin;
-/// 
-/// let Ok(n) = read_stdin::<i32>() else {
+/// use read_stdin;
+///
+/// let Ok(n) = read_stdin::once::<i32>() else {
 ///     println!("You entered an incorrect data type!");
 ///     return;
 /// };
-/// 
+///
 /// println!("You entered: {}", n)
 /// ```
-pub fn read_stdin<T>() -> Result<T, <T as FromStr>::Err>
+pub fn once<T>() -> Result<T, <T as FromStr>::Err>
 where
     T: FromStr + 'static,
 {
@@ -38,21 +41,21 @@ where
 
 /// Read user input from `stdin` and try to parse it into a generic type. I will keep prompting
 /// the user for input until they enter data that properly parses.
-/// 
+///
 /// # Example
 /// ```rust
-/// use read_stdin::read_stdin_until_ok;
-/// 
-/// let n = read_stdin_until_ok::<i32>();
-/// 
+/// use read_stdin;
+///
+/// let n: i32 = read_stdin::until_ok();
+///
 /// println!("You entered: {}", n)
 /// ```
-pub fn read_stdin_until_ok<T>() -> T
+pub fn until_ok<T>() -> T
 where
     T: FromStr + 'static,
 {
     loop {
-        match read_stdin::<T>() {
+        match once::<T>() {
             Ok(value) => return value,
             Err(_) => {
                 println!("Failed to parse. Please input a correct data type.");
@@ -60,4 +63,71 @@ where
             }
         }
     }
+}
+
+/// Creates a CLI prompt and asks the user for input.
+///
+/// # Example
+/// ```rust
+/// use read_stdin;
+///
+/// let n: i32 = read_stdin::prompt("Enter a number: ");
+///
+/// println!("You entered: {}", n)
+/// ```
+pub fn prompt<T>(prompt: &(impl ToString + Display + ?Sized)) -> Result<T, <T as FromStr>::Err>
+where
+    T: FromStr + 'static,
+{
+    let mut stdout = stdout().lock();
+
+    print!("{}", prompt);
+    stdout.flush().expect("Failed to flush stdout");
+    once()
+}
+
+/// Creates a CLI prompt and repeatetly asks the user for input until their input is of valid type.
+///
+/// # Example
+/// ```rust
+/// use read_stdin;
+///
+/// let n: i32 = read_stdin::prompt("Enter a number: ");
+///
+/// println!("You entered: {}", n)
+/// ```
+pub fn prompt_until_ok<T>(prompt: &(impl ToString + Display + ?Sized)) -> T
+where
+    T: FromStr + 'static,
+{
+    let mut stdout = stdout().lock();
+
+    loop {
+        print!("{}", prompt);
+        stdout.flush().expect("Failed to flush stdout");
+        match once::<T>() {
+            Ok(value) => return value,
+            Err(_) => {
+                println!("Failed to parse. Please input a correct data type.");
+                continue;
+            }
+        }
+    };
+}
+
+// Compatibility
+#[deprecated(since = "1.1.0", note = "Use `until_ok` instead.")]
+pub fn read_stdin_until_ok<T>() -> T
+where
+    T: FromStr + 'static,
+{
+    until_ok()
+}
+
+#[deprecated(since = "1.1.0", note = "Use `once` instead.")]
+pub fn read_stdin<T>() -> Result<T, <T as FromStr>::Err>
+where
+    T: FromStr + 'static,
+{
+    once()
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,10 @@
 #[cfg(test)]
 mod tests {
-    use crate::{read_stdin_until_ok, read_stdin};
+    use crate::{once, until_ok, prompt, prompt_until_ok};
 
     #[test]
     fn get_u32() {
-        let Ok(n) = read_stdin::<u32>() else {
+        let Ok(n) = once::<u32>() else {
             panic!("Incorrect data type!");
         };
         assert_eq!(n, 42);
@@ -12,7 +12,7 @@ mod tests {
 
     #[test]
     fn get_i32() {
-        let Ok(n) = read_stdin::<i32>() else {
+        let Ok(n) = once::<i32>() else {
             panic!("Incorrect data type!");
         };
         assert_eq!(n, -42);
@@ -20,13 +20,27 @@ mod tests {
 
     #[test]
     fn get_u32_loop() {
-        let n = read_stdin_until_ok::<u32>();
+        let n = until_ok::<u32>();
         assert_eq!(n, 42);
     }
 
     #[test]
     fn get_i32_loop() {
-        let n = read_stdin_until_ok::<i32>();
+        let n = until_ok::<i32>();
         assert_eq!(n, -42);
+    }
+
+    #[test]
+    fn get_i32_prompt() {
+        let result = prompt::<i32>("Enter 42: ");
+
+        assert_eq!(result.unwrap(), 42);
+    }
+    
+    #[test]
+    fn get_i32_prompt_until_ok() {
+        let result = prompt_until_ok::<i32>("Enter 42: ");
+
+        assert_eq!(result, 42);
     }
 }
